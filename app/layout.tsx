a78fa8a88c831501cd105cdae125d29742346f4c
Thipakorn K. <gameboyfun@hotmail.com>
2025-06-12 09:39:38 +0700

    ✨ : add analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from './providers'
 import { siteConfig } from '@/config/site'
 import { fontSans } from '@/config/fonts'
 import BgmPlayer from '@/components/bgmPlayer'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: {
@@ -38,9 +39,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           <div className='relative flex flex-col min-h-screen'>
-            <main className='container mx-auto max-w-7xl pt-16 px-6 flex-grow'>
+            <main className='container mx-auto max-w-7xl pt-16 px-6 pb-6 flex-grow'>
               {/* <BgmPlayer /> */}
               {children}
+              <Analytics />
             </main>
             <footer className='w-full flex items-center justify-center py-3'>
               <Link
